01811210f3a2829e711c226a426800baf8f82073
heidsoft <heidsoft@sina.com>
2017-04-06 14:26:57 +0800

    Update

--- README.md
+++ README.md
@@ -22,6 +22,25 @@ https://www.apachefriends.org/zh_cn/download.html
 npm install --save redux
 npm install --save react-redux
 npm install --save-dev redux-devtools
+
+
+"predeploy": "npm run build",
+"deploy": "gh-pages -d build"
+npm install --save-dev gh-pages
+npm run deploy
+
+http://wiki.jikexueyuan.com/project/react/tutorial.html
+http://www.ruanyifeng.com/blog/2015/03/react.html
+http://www.infoq.com/cn/articles/subversion-front-end-ui-development-framework-react
+http://www.jianshu.com/p/0e42799be566
+http://redux.js.org/
+https://segmentfault.com/a/1190000003503338?_ea=323420
+http://www.cnblogs.com/Leo_wl/p/4780750.html
+http://www.jianshu.com/p/3334467e4b32
+http://www.oschina.net/news/72579/redux-react
+https://segmentfault.com/a/1190000006055973
+https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md
+https://ant.design/docs/react/practical-projects-cn
 ``
 
 #jsx
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DatePicker, message } from 'antd';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('example')
-);
+
+// ReactDOM.render(
+//     <h1>Hello, world!</h1>,
+//     document.getElementById('example')
+// );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: '',
+        };
+    }
+    handleChange(date) {
+        message.info('您选择的日期是: ' + date.toString());
+        this.setState({ date });
+    }
+    render() {
+        return (
+            <div style={{ width: 400, margin: '100px auto' }}>
+                <DatePicker onChange={value => this.handleChange(value)} />
+                <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('example'));
