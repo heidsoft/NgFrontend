da9c11c1065471b647b00673924ce51dfc9e7ae7
heidsoft <heidsoft@sina.com>
2017-04-07 13:59:40 +0800

    Update

--- app/src/index.js
+++ app/src/index.js
@@ -1,33 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DatePicker, message } from 'antd';
-import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+import './App.css';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
 
-
-// ReactDOM.render(
-//     <h1>Hello, world!</h1>,
-//     document.getElementById('example')
-// );
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: '',
-        };
-    }
-    handleChange(date) {
-        message.info('您选择的日期是: ' + date.toString());
-        this.setState({ date });
-    }
-    render() {
-        return (
-            <div style={{ width: 400, margin: '100px auto' }}>
-                <DatePicker onChange={value => this.handleChange(value)} />
-                <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
-            </div>
-        );
-    }
+class SiderDemo extends React.Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    return (
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('example'));
+ReactDOM.render(<SiderDemo />, document.getElementById('example'));
+
+
+
