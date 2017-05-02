0860db12a53f709e97803194d73f519df8b0508d
heidsoft <heidsoft@sina.com>
2017-05-02 09:08:43 +0800

    Update 更新项目

--- ant-app/src/index.js
+++ ant-app/src/index.js
@@ -25,3 +25,65 @@ app.router(require('./router'));
 
 // 5. Start
 app.start('#root');
+
+
+let add = (a,b)=> a+b;
+
+console.log(add);
+
+console.log(add(1,2));
+
+let numbers = [1,2,3];
+let doubleNumbers = numbers.map((number) => number * 2);
+console.log(doubleNumbers);
+
+var age = 2;
+//在对象方法的嵌套函数中，this会指向global对象
+let showMe1 = {
+  age:1,
+  grow:function(){
+    setTimeout(function(){
+      console.log("--------showMe1-start--------");
+      console.log(++this.age);
+      console.log("--------showMe1-end--------\n");
+    },100);
+  }
+};
+
+showMe1.grow();
+
+
+
+let showMe2 = {
+  age:1,
+  grow:function(){
+    const self = this;
+    setTimeout(function(){
+      console.log("--------showMe2-start--------");
+      console.log(++self.age);
+      console.log("--------showMe2-end--------\n");
+    },100);
+  }
+};
+
+showMe2.grow();
+
+
+let showMe3 = {
+  age:1,
+  grow:function(){
+    setTimeout(()=>{
+      console.log("--------showMe3-start--------");
+      console.log(++this.age);
+      console.log("--------showMe3-end--------\n");
+    },100);
+  }
+};
+showMe3.grow();
+
+function desc(name="heidsoft",age=18){
+  return name + ' is ' + age + ' years old';
+}
+
+console.log(desc());
+console.log(desc('jake',20));
--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,6 @@ import {
     Link
 } from 'react-router-dom';
 import './App.css';
-import UserAdd from './UserAdd'
 import { Layout, Menu, Icon } from 'antd';
 
 const { Header, Sider, Content } = Layout;
@@ -14,6 +13,97 @@ const { Header, Sider, Content } = Layout;
 console.log(Layout)
 console.log(Header)
 console.log(Content)
+
+/**
+ * 在React中处理表单有些不一样，由于React提倡“单向数据流”，
+ * React中的表单并不提供双向数据绑定的功能，我们需要给表单绑定它的value，
+ * 然后提供一个onChange的处理方法来更新value的值。
+ * 这里我们使用组件的state来维护表单的值，在onChange的时候使用setState来更新值，
+ * 最后，在表单提交事件被触发的时候，我们输出state来观察最终获得的表单值：
+ */
+class UserAdd extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            name: '',
+            age: 0,
+            gender: ''
+        };
+    }
+    handleValueChange (field, value, type = 'string') {
+        // 由于表单的值都是字符串，我们可以根据传入type为number来手动转换value的类型为number类型
+        if (type === 'number') {
+            value = +value;
+        }
+
+        this.setState({
+            [field]: value
+        });
+    }
+    handleSubmit (e) {
+        e.preventDefault();
+
+        const {name, age, gender} = this.state;
+        fetch('http://localhost:4000/user', {
+            method: 'post',
+            // 使用fetch提交的json数据需要使用JSON.stringify转换为字符串
+            body: JSON.stringify({
+                name,
+                age,
+                gender
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                // 当添加成功时，返回的json对象中应包含一个有效的id字段
+                // 所以可以使用res.id来判断添加是否成功
+                if (res.id) {
+                    alert('添加用户成功');
+                    this.setState({
+                        name: '',
+                        age: 0,
+                        gender: ''
+                    });
+                } else {
+                    alert('添加失败');
+                }
+            })
+            .catch((err) => console.error(err));
+    }
+    render () {
+        const {name, age, gender} = this.state;
+        return (
+            <div>
+                <header>
+                    <h1>添加用户</h1>
+                </header>
+
+                <main>
+                    <form onSubmit={(e) => this.handleSubmit(e)}>
+                        <label>用户名：</label>
+                        <input type="text" value={name} onChange={(e) => this.handleValueChange('name', e.target.value)}/>
+                        <br/>
+                        <label>年龄：</label>
+                        <input type="number" value={age || ''} onChange={(e) => this.handleValueChange('age', e.target.value, 'number')}/>
+                        <br/>
+                        <label>性别：</label>
+                        <select value={gender} onChange={(e) => this.handleValueChange('gender', e.target.value)}>
+                            <option value="">请选择</option>
+                            <option value="male">男</option>
+                            <option value="female">女</option>
+                        </select>
+                        <br/>
+                        <br/>
+                        <input type="submit" value="提交"/>
+                    </form>
+                </main>
+            </div>
+        );
+    }
+}
 
 class SiderDemo extends React.Component {
   state = {
@@ -31,6 +121,7 @@ class SiderDemo extends React.Component {
           trigger={null}
           collapsible
           collapsed={this.state.collapsed}
+          style={{ height:'900px' }}
         >
           <div className="logo" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
@@ -114,17 +205,10 @@ const Topic = ({ match }) => (
 ReactDOM.render((
     <Router>
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/topics">Topics</Link></li>
-            </ul>
-
-            <hr/>
-
             <Route exact path="/" component={SiderDemo}/>
             <Route path="/about" component={About}/>
             <Route path="/topics" component={Topics}/>
+            <Route path="/user/add" component={UserAdd}/>
         </div>
     </Router>
 ), 
